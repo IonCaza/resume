91ff7fd321cccc1bb75c9bd5e3195cddd673c3d3
Ion Caza <contact@johncaza.com>
2017-06-08 21:41:04 -0400

    Add various changes including node server
    Run npm install / node index.js

--- javascripts/site.js
+++
@@ -1,5 +0,0 @@
-function gmtl(lhs, rhs, subject) {
-    document.write("<a property=\"schema:email\" class=\"email\" href=\"mailto");
-    document.write(":" + lhs + "@");
-    document.write(rhs + "?subject=" + subject + " \">" + lhs + "@" + rhs + "<\/a>");
-};
