7009d64f5d690e002607b0f9a0613eec6dfb5665
Dev Caza <dev@ATEX.US>
2018-07-26 16:15:09 -0400

    Achieves working create react app + webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { clientSrcPath, assetsBuildPath, publicSrcPath } = require('./paths')();
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/assets/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: [publicSrcPath],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        exclude: [publicSrcPath],
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin]
+};
