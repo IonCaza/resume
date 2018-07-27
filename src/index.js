fdc3465fc17c57c86ffd44b95d4f1728640a2f6e
Ion Caza <contact@johncaza.com>
2018-07-27 13:43:18 -0400

    Cleans up index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import App from './components/App';
 import Layout from './components/Layout';
-
-import Header from './components/Header';
-
 
 import './styles/index.css';
 import './assets/favicon.ico';
-import { Switch } from '../node_modules/@material-ui/core';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render((
     <BrowserRouter>
         <Layout />
     </BrowserRouter>
   ), document.getElementById('root'))
-// registerServiceWorker();
+registerServiceWorker();
