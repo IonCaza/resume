16ec328640d296771c2dbaac9b1c729f1cee63ac
Ion Caza <contact@johncaza.com>
2018-07-30 20:26:12 -0400

    Adds changes to header, new AppBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import PrintIcon from '@material-ui/icons/Print';
+import SaveIcon from '@material-ui/icons/SaveAltSharp';
 import '../styles/Header.scss';
 
 import withRoot from './withRoot';
 
-const styles = theme => ({
+const styles = () => ({
   root: {
-    textAlign: 'center',
-    paddingTop: theme.spacing.unit * 10,
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: 0,
+    marginRight: 0,
   },
 });
 
@@ -18,9 +30,22 @@ const Header = props => {
 
   return (
     <div className={classes.root}>
-      <Typography variant="display1" gutterBottom>
-        Not something it is not
-      </Typography>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            Ion Caza&apos;s Resume
+          </Typography>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Print">
+            <PrintIcon />
+          </IconButton>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Download PDF">
+            <SaveIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
