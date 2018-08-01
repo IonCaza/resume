158070918b8ae3aa79b67c4690bcdb9e837e9222
Ion Caza <contact@johncaza.com>
2018-07-31 22:25:30 -0400

    Adds Drawer, working this time

---
+++ src/components/Navigation.js
@@ -0,0 +1,257 @@
+import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/MenuSharp';
+import PrintIcon from '@material-ui/icons/PrintSharp';
+import SaveIcon from '@material-ui/icons/SaveAltSharp';
+
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import StarIcon from '@material-ui/icons/Star';
+import SendIcon from '@material-ui/icons/Send';
+import MailIcon from '@material-ui/icons/Mail';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ReportIcon from '@material-ui/icons/Report';
+
+import App from './App';
+import withRoot from './withRoot';
+
+export const mailFolderListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <InboxIcon />
+      </ListItemIcon>
+      <ListItemText primary="Inbox" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <StarIcon />
+      </ListItemIcon>
+      <ListItemText primary="Starred" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <SendIcon />
+      </ListItemIcon>
+      <ListItemText primary="Send mail" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <DraftsIcon />
+      </ListItemIcon>
+      <ListItemText primary="Drafts" />
+    </ListItem>
+  </div>
+);
+
+export const otherMailFolderListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <MailIcon />
+      </ListItemIcon>
+      <ListItemText primary="All mail" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <DeleteIcon />
+      </ListItemIcon>
+      <ListItemText primary="Trash" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <ReportIcon />
+      </ListItemIcon>
+      <ListItemText primary="Spam" />
+    </ListItem>
+  </div>
+);
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  appFrame: {
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  flex: {
+    flexGrow: 1,
+    // textAlign: 'center',
+  },
+  drawer: {
+    position: 'fixed',
+    top: 0,
+  },
+  appBar: {
+    position: 'fixed',
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  'appBarShift-left': {
+    marginLeft: drawerWidth,
+  },
+  menuButton: {
+    marginLeft: 0,
+    marginRight: 10,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  'content-left': {
+    marginLeft: 0,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  'contentShift-left': {
+    marginLeft: drawerWidth,
+  },
+});
+
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drawerIsOpen: false,
+    };
+    this.closeDrawer = this.toggleDrawer.bind(this, false);
+    this.openDrawer = this.toggleDrawer.bind(this, true);
+  }
+
+  toggleDrawer(tf) {
+    this.setState({
+      drawerIsOpen: tf,
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { drawerIsOpen } = this.state;
+
+    const drawer = (
+      <Drawer
+        variant="persistent"
+        anchor="left"
+        open={drawerIsOpen}
+        className={classes.drawer}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={this.closeDrawer}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>{mailFolderListItems}</List>
+        <Divider />
+        <List>{otherMailFolderListItems}</List>
+      </Drawer>
+    );
+
+    return (
+      <div className={classes.root}>
+        <div className={classes.appFrame}>
+          <AppBar
+            className={classNames(classes.appBar, {
+              [classes.appBarShift]: drawerIsOpen,
+              [classes['appBarShift-left']]: drawerIsOpen,
+            })}
+          >
+            <Toolbar disableGutters={!drawerIsOpen}>
+              <IconButton
+                className={classNames(classes.menuButton, drawerIsOpen && classes.hide)}
+                color="inherit"
+                aria-label="Menu"
+                onClick={this.openDrawer}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit" className={classes.flex}>
+                Ion Caza&apos;s Resume
+              </Typography>
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Print">
+                <PrintIcon />
+              </IconButton>
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Download PDF">
+                <SaveIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          {drawer}
+          <main
+            className={classNames(classes.content, classes['content-left'], {
+              [classes.contentShift]: drawerIsOpen,
+              [classes['contentShift-left']]: drawerIsOpen,
+            })}
+          >
+            <div className={classes.drawerHeader} />
+            <Switch>
+              <Route path="/" exact component={App} />
+              <Redirect to="/" />
+            </Switch>
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+Navigation.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withRoot(withStyles(styles)(Navigation));
